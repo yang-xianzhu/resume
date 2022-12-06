5eb6e18bb8cbafb233d12845e8d6883373f2df4c
yang-xianzhu <2416548256@qq.com>
2022-12-07 00:17:40 +0800

    update

--- meta.config.js
+++ meta.config.js
@@ -1,5 +1,10 @@
 export default {
-  title:'个人简历',
-  skills: ["VueJS", "ReactJS", "TypeScript", "JS", "SolidJS"],
-  name: "杨某",
+  title: "个人简历",
+  skills: ["JavaScript", "VueJS", "ReactJS", "TypeScript"],
+  name: "杨先铸",
+  position: "前端工程师",
+  workExperience: "3年工作经验",
+  wx: "yang2416548256",
+  qq: "2416548256@qq.com",
+  phone: "18476472710",
 };
